10cfdd921eac87b04d7e76862e213cbf7ae2d40b
Zakir <zakir@cefalo.com>
2021-11-26 18:21:14 +0600

    Added surefire and jacoco.

---
+++ src/test/java/com/example/demo/service/StudentServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceTest {
+
+    @Test
+    void testStudentService() {
+        assertNotNull(new Object());
+    }
+}
